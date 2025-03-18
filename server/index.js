e076c7ff52b2771becf46b5368d5621815d936f6
B-Manish <batchumanish@gmail.com>
2025-03-18 22:37:54 +0530

    Added mongodb local instance connection string

--- server/index.js
+++ server/index.js
@@ -7,18 +7,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Sample route
+
 app.get('/', (req, res) => {
   res.send('Welcome to the MERN app backend');
 });
 
 // Database connection
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
